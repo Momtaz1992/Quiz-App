e65864caed3c6fe1bb5eea5dbd0ced23caa78c87
Momtaz Asmar <122457138+Momtaz1992@users.noreply.github.com>
2023-11-08 19:00:49 +0100

    production going through

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ const questions = [
 ]
 
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answear-buttons");
+const answerButtons = document.getElementById("answear-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
@@ -57,6 +57,7 @@ function startQuiz(){
 }
 
 function showQuestion(){
+       resetState();
        let currentQuestion = questions[currentQuestionIndex];
        let questionNo = currentQuestionIndex + 1;
        questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -65,7 +66,13 @@ function showQuestion(){
               const button = document.createElement("button");
               button.innerHTML = answer.text;
               button.classList.add("btn");
-              answerButton.appendChild(button);
+              answerButtons.appendChild(button);
        })
+}
+function resetState(){
+       nextButton.style.display="none";
+       while(answerButtons.firstChild){
+              answerButtons.removeChild(answerButtons.firstChild);
+       }
 }
 startQuiz();
